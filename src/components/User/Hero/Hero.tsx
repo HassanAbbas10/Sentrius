bb108eb28d3a3c660f80e308a349ada12d22e839
HassanAbbas10 <hassanabbas05674@gmail.com>
2025-01-22 17:06:16 +0500

    new file:   src/components/User/Hero/Hero.tsx 	modified:   src/pages/Home.tsx

---
+++ src/components/User/Hero/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero : React.FC = () => {
+  return (
+    <div>
+        <main className='bg-background-tertiary min-h-96'>
+.
+        </main>
+    </div>
+  )
+}
+
+export default Hero
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
-
+import Hero from '../components/User/Hero/Hero'
 const Home : React.FC = () => {
   return (
-    <main className="flex-grow bg-gray-100">
-        <div className="container mx-auto px-4 py-8">
-          <h1 className="text-4xl font-bold mb-4 text-gray-800">Welcome to Our Website</h1>
-          <p className="text-lg text-gray-600">This is the main content of your home page.</p>
-        </div>
+    <main className="min-h-fit bg-gray-100">
+    <Hero/>
       </main>
   )
 }
