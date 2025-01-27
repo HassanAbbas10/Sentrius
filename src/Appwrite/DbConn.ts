ce8e1232bc349952029bf5e3d349d7132ff10153
HassanAbbas10 <hassanabbas05674@gmail.com>
2025-01-27 21:28:34 +0500

    modified:   src/Appwrite/DbConn.ts

--- src/Appwrite/DbConn.ts
+++ src/Appwrite/DbConn.ts
@@ -1,11 +1,19 @@
-import { Client } from 'appwrite';
-const client = new Client();
-const apiKey = import.meta.env.;
+import { Client, Account } from "appwrite";
 
-client.setProject(apiKey);
-if(apiKey === undefined){
-    throw new Error;
+
+const projectID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const apiKey = import.meta.env.VITE_APPWRITE_SECRET_KEY;
+const projectEndpoint = import.meta.env.VITE_APPWRITE_ENDPOINT;
+const client = new Client()
+.setEndpoint(projectEndpoint).
+setProject(projectID);
+
+
+if (apiKey === undefined) {
+  throw new Error();
+} else {
+  console.log("Hey Homie I am Tony");
 }
-else{
-    console.log("Hey Homie I am Tony")
-}
+
+export const account = new Account(client);
+export { ID } from "appwrite";
